11b60427b0dde415c4693484b2b32c5d7b231eb9
Zachary Mixon <zmixon@vynyl.com>
2016-12-07 12:36:56 -0700

    Update CompletedTodos component

--- src/components/CompletedTodos.vue
+++ src/components/CompletedTodos.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="completed-todos">
-    <h3></h3>
+    <h3 v-if="completed.length">Completed({{completed.length}})</h3>
     <ul class="list-group">
-      <li class="list-group-item">
+      <li class="list-group-item" v-for="todo in completed">
+      {{todo.body}}
       <button type="button" @click="remove(todo)" class="btn btn-default btn-sm"></button>
       <span class="glyphicon glyphicon-remove-circle"></span>&nbsp;Remove
       </li>
