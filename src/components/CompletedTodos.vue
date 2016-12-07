29e28fdbb66fbca2078a24dffc477bdc7856dcda
Zachary Mixon <zmixon@vynyl.com>
2016-12-07 11:55:24 -0700

    Add CompletedTodos component

---
+++ src/components/CompletedTodos.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="completed-todos">
+    <h3></h3>
+    <ul class="list-group">
+      <li class="list-group-item">
+      <button type="button" @click="remove(todo)" class="btn btn-default btn-sm"></button>
+      <span class="glyphicon glyphicon-remove-circle"></span>&nbsp;Remove
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      remove(todo) {
+        this.$store.dispatch('removeTodo', todo);
+      },
+    },
+    computed: {
+      completed() {
+        return this.$store.getters.completedTodos;
+      },
+    },
+  };
+</script>
