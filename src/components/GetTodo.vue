ab0ee84d08ebff93c641aa0b40a97ab1f0e2d226
Zachary Mixon <zmixon@vynyl.com>
2016-12-07 11:44:18 -0700

    Add GetTodo component

---
+++ src/components/GetTodo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="get-todo" class="container">
+    <input type="text" class="form-control">
+    <button class="btn btn-primary"></button>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      getTodo(e) {
+        this.$store.dispatch('getTodo', e.target.value);
+      },
+      addTodo() {
+        this.$store.dispatch('addTodo');
+        this.$store.dispatch('clearTodo');
+      },
+    },
+    computed: {
+      newTodo() {
+        return this.$store.getters.newTodo;
+      },
+    },
+  };
+</script>
